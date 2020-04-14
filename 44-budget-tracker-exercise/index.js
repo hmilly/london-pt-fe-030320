17659cb69a3a268322a1a358aeed6e580d95faa7
Holly <61398991+hmilly@users.noreply.github.com>
2020-04-14 23:55:48 +0100

    Update index.js
    All working in my eyes, but not testing right maybe?

--- 44-budget-tracker-exercise/index.js
+++ 44-budget-tracker-exercise/index.js
@@ -1,37 +1,37 @@
+//Hi, I think I passed all the tests on this one, but some don' work! Thanks!
+
 const productsContainer = document.querySelector("#products");
 let count = 0;
 let zero = -0.00;
-let remainingBudget = document.querySelector("#remaining > span").innerText.slice(1, 6)
- 
-console.log(remainingBudget)
- 
-for (i in products) {
- 
+let budget = document.querySelector("#remaining > span").innerText.slice(1)
+
+for (let i in products) {
   let container = document.createElement("div");
- 
   let img = document.createElement("img")
   img.src = `${products[i].img}`;
   container.appendChild(img)
- 
+
   let h3 = document.createElement("h3")
   h3.innerText = `${products[i].name}`;
   container.appendChild(h3);
- 
+
   let p = document.createElement("p")
   p.innerText = `${products[i].price}`
   container.appendChild(p);
- 
+
   let select = document.createElement("select")
   select.id = `${products[i].id}`;
- 
+
   select.addEventListener("change", (e) => {
-    let money = (remainingBudget -= e.target.value).toFixed(2);
+    console.log(e.target.value)
+    let money = (budget -= e.target.value).toFixed(2);
     if (money <= 0) {
+      e.target.disabled = true;
       let times = () => {
         let rem = document.querySelector("#remaining")
         let cont = document.createElement("div");
         cont.className = `error`;
-        cont.innerText = ">Not enough money left for that!"
+        cont.innerText = "Not enough money left for that!"
         rem.appendChild(cont)
       }
       setTimeout(times(), 3000)
@@ -40,13 +40,12 @@ for (i in products) {
       moneyDiv.innerText = `£${money}`
     }
   })
- 
   container.appendChild(select);
- 
+
   let option = document.createElement("option")
   option.innerText = `${count++}`
   select.appendChild(option)
- 
+
   while (products[i].max_quantity !== 0) {
     let opt = document.createElement("option")
     opt.innerText = `${count++}`;
@@ -54,21 +53,7 @@ for (i in products) {
     select.appendChild(opt)
     products[i].max_quantity--;
   }
- 
+
   productsContainer.appendChild(container);
   count = 0;
 }
- 
- 
-/*
- 
-displayed for 3 seconds and the select field doesn't
-retain the selected value
-  >>>>>>>>>> mine wont time out ??
-
-Remaining budget should update when user selects new quantity
-
-gergana said to add this back but it hurts my head so will leave it for now.
-*/
- 
-
