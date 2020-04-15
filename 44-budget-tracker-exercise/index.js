ee9777fc20c8793ea087704513e7ad1edd516f3c
Holly <61398991+hmilly@users.noreply.github.com>
2020-04-15 18:25:19 +0100

    Update index.js
    Updated selection event listener so it works correctly and budget is displayed correctly whatever is picked

--- 44-budget-tracker-exercise/index.js
+++ 44-budget-tracker-exercise/index.js
@@ -1,55 +1,60 @@
-//Hi, I think I passed all the tests on this one, 3 in the middle werent passing Thanks!
+//Hi, I think I defo passed number 2 3 and 4 but not passing in my console.. Thanks 
 
 const productsContainer = document.querySelector("#products");
 let count = 0;
-let budget = document.querySelector("#remaining > span").innerText.slice(1)
+let budget = document.querySelector("#remaining > span").innerText;
+let money = budget.slice(1);
 
 for (let i in products) {
   let container = document.createElement("div");
-  let img = document.createElement("img")
+  let img = document.createElement("img");
   img.src = `${products[i].img}`;
-  container.appendChild(img)
+  container.appendChild(img);
 
-  let h3 = document.createElement("h3")
+  let h3 = document.createElement("h3");
   h3.innerText = `${products[i].name}`;
   container.appendChild(h3);
 
-  let p = document.createElement("p")
-  p.innerText = `${products[i].price}`
+  let p = document.createElement("p");
+  p.innerText = `${products[i].price}`;
   container.appendChild(p);
 
-  let select = document.createElement("select")
+  let select = document.createElement("select");
   select.id = `${products[i].id}`;
 
   select.addEventListener("change", (e) => {
-    console.log(e.target.value)
-    let money = (budget -= e.target.value).toFixed(2);
+    let moneyDiv = document.querySelector("#remaining > span");
+    console.log(e.target.value);
+    money = (money -= e.target.value).toFixed(2);
+    moneyDiv.innerText = `£${money}`;
     if (money <= 0) {
       e.target.disabled = true;
-      let times = () => {
-        let rem = document.querySelector("#remaining")
+      let connectingDiv = document.querySelector("#remaining");
+      setTimeout(() => {
         let cont = document.createElement("div");
         cont.className = `error`;
-        cont.innerText = "Not enough money left for that!"
-        rem.appendChild(cont)
-      }
-      setTimeout(times(), 3000)
-    } else {
-      let moneyDiv = document.querySelector("#remaining > span")
-      moneyDiv.innerText = `£${money}`
+        cont.innerText = "Not enough money left for that!";
+        connectingDiv.appendChild(cont);
+        money = (Number(e.target.value) + Number(money)).toFixed(2);
+        moneyDiv.innerText = `£${money}`;
+        setTimeout(() => {
+          let newDiv = document.querySelector(".error");
+          connectingDiv.removeChild(newDiv);
+        }, 4000);
+      }, 2000);
     }
-  })
+  });
   container.appendChild(select);
 
-  let option = document.createElement("option")
-  option.innerText = `${count++}`
-  select.appendChild(option)
+  let option = document.createElement("option");
+  option.innerText = `${count++}`;
+  select.appendChild(option);
 
   while (products[i].max_quantity !== 0) {
-    let opt = document.createElement("option")
+    let opt = document.createElement("option");
     opt.innerText = `${count++}`;
     opt.value = ((count - 1) * products[i].price).toFixed(2);
-    select.appendChild(opt)
+    select.appendChild(opt);
     products[i].max_quantity--;
   }
 
