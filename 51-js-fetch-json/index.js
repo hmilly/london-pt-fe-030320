14be1fe5c6b53b83d2974087d142c55115234483
Holly <61398991+hmilly@users.noreply.github.com>
2020-05-07 15:34:40 +0100

    Works, doesnt pass tests !

--- 51-js-fetch-json/index.js
+++ 51-js-fetch-json/index.js
@@ -9,6 +9,11 @@ const input = document.querySelector("input");
 // run `npm install -g serve`
 // then run `serve` in this directory
 
+
+button.addEventListener("click", () => {
+    fetchData("https://cat-fact.herokuapp.com/facts")
+})
+
 /**
  * Exercise 1
  *
@@ -16,10 +21,19 @@ const input = document.querySelector("input");
  * a URL as an argument and sends a GET request.
  * When you get a response, return an array of facts.
  */
-
-
+let fetchData = (url) => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+                let arr = data.all.map(item => item.text).sort(() => 0.5 - Math.random());
+                let selected = arr.slice(0, 3);
+                result.innerHTML = `<p>${selected[0]}</p><p>${selected[1]}</p><p>${selected[2]}</p>`;
+            })
+}
 /**
  * Description of the application:
+ *
+ *
  *
  * As a user I should be able to:
  * 1. click on a button "Get random facts"
