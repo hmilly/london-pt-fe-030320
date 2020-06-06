5125c7cbb2a32734f029dd9fa4a7e23ca060b541
Holly <61398991+hmilly@users.noreply.github.com>
2020-06-06 14:58:25 +0100

    Fixed hardcoded date

--- 55-react-component-list/src/App.js
+++ 55-react-component-list/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 
 const APP_TITLE = "Awesome Pet Shop";
 
-
 /**
  * Exercise: 1
  *
@@ -15,20 +14,33 @@ const APP_TITLE = "Awesome Pet Shop";
  * 2. Display today's date in a p
  */
 
+const TodaysDate = () => {
+  const todayObj = new Date();
+  let day = todayObj.getDay();
+  let month = todayObj.getMonth() + 1;
+  const year = todayObj.getFullYear();
+
+  if (month < 10) month = `0${month}`;
+  if (day < 10) day = `0${day}`;
+
+  const today = `${day}/${month}/${year}`;
+  return <p className="date">{today}</p>;
+};
+
 const App = () => {
   const animals = ["cat", "dog", "giraffe", "pig", "lion", "rabbit"];
 
-  return (<div className="app">
-    <h1>{APP_TITLE}</h1>
-    <p>02/06/2020</p>
-    <ul>
-      {animals.map((item) => (
-       <li key={item}>{item}</li> 
-      ))}
-    </ul>
-
-  </div>
-  )}
+  return (
+    <div className="app">
+      <h1>{APP_TITLE}</h1>
+      <div>{TodaysDate()}</div>
+      <ul>
+        {animals.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
-
